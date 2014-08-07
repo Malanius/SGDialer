2d36f71ef02c87ac20afe88378a7a30936b82210
Malanius <malanius@seznam.cz>
2014-08-07 21:08:55 +0200

    Output messages tweak

--- src/sgdialer/Verifier.java
+++ src/sgdialer/Verifier.java
@@ -1,36 +1,37 @@
 package sgdialer;
 
 /**
- *
+ * Creates the symbol array and is responsible for adress verification
  * @author Malanius
  * @version 0.0
  */
 class Verifier {
 
-    private char[] symbols; //Symbols array
+    private char[] symbols = new char[38]; //Symbols array
     private String message; //Output message of operations
 
     //Constructor
     public Verifier() {
-        symbols = new char[38];
         //Fills the symbols array with alphabet characters
         int chr = (int) 'a'; //Integer for character, begining at a
         for (int i = 0; i <= 25; i++) {
             symbols[i] = (char) (chr + i);
             //Message for testing
-            System.out.println(String.format("Writed character %s to array on pos %s...", (char) (chr + i), i));
+            System.out.println(String.format("[Verifier]: Writed character %s to array on pos %s...", (char) (chr + i), i));
 
         }
         //Fills the character array with number characters
-        chr = (int)'0'; //Sets the char to zero
+        chr = (int) '0'; //Sets the char to zero
         for (int i = 26; i <= 35; i++) {
-            symbols[i] = (char)chr;
+            symbols[i] = (char) chr;
             //Message for testing
-            System.out.println(String.format("Writed character %s to array on pos %s...", (char) chr, i));
+            System.out.println(String.format("[Verifier]: Writed character %s to array on pos %s...", (char) chr, i));
             chr++;
         }
         symbols[36] = '-';
         symbols[37] = '.';
+        System.out.print(String.format("[Verifier]: Created array of %s symbols: ", symbols.length));
+        System.out.println(writeSymbols());
     }//Constructor end
 
     /**
@@ -45,6 +46,14 @@ class Verifier {
      */
     public String getMessage() {
         return message;
+    }
+
+    private String writeSymbols() {
+        String strSymb = "";
+        for (int o = 0; o < symbols.length; o++) {
+            strSymb += symbols[o];
+        }
+        return strSymb;
     }
 
     protected boolean verify() {
