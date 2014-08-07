f5c5951054109b3cf81bf85e50a954d984415b4d
Malanius <malanius@seznam.cz>
2014-08-07 21:35:02 +0200

    Symbol array pass

--- src/sgdialer/Dialer.java
+++ src/sgdialer/Dialer.java
@@ -6,5 +6,16 @@ package sgdialer;
  * @version 0.0
  */
 public class Dialer {
-    
+    private char symbols[] = new char[38];//Symbols array
+    //Consrtructor
+    public Dialer(char[] symbols){
+        this.symbols = symbols;
+        //Testing message
+        System.out.print("[Dialer]: Sucessfully obtained symbol array: ");
+        String strSymbols = "";
+        for (int i = 0; i < this.symbols.length; i++){
+            strSymbols += this.symbols[i]; 
+        }
+        System.out.println(strSymbols);
+    }//End of Constructor
 }
