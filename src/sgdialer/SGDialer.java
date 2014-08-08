28fb0ad011701928bdc611b4d6da2182bbe0f11a
Malanius <malanius@seznam.cz>
2014-08-08 21:43:04 +0200

    Finished basic logic

--- src/sgdialer/SGDialer.java
+++ src/sgdialer/SGDialer.java
@@ -1,6 +1,7 @@
 package sgdialer;
 
 import java.util.Scanner;
+
 /**
  *
  * @author Malanius
@@ -25,12 +26,28 @@ public class SGDialer {
         System.err.println("[Main]: Creating scanner.");
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the dialing computer. Please note that valid symbols are: " + symbolArray.writeSymbols());
-        System.out.println("The point of origin symbol is \".\"");
+        System.out.println("The point of origin symbol is \"#\"");
         System.out.print("Insert adress to dial: ");
         String adress = sc.nextLine();
-        
+        if (adress.equals("exit")) {//Exit command chceck
+            System.err.println("[Main]: Recevied shutdown command...");
+            System.out.println("Terminating dial program...");
+            System.exit(0);
+        } else {//No exit command recevied
+            //Verify adress
+            if (verifier.verify(adress)) {//Correct adress is dialed
+                System.out.println("Dialing " + adress);
+                dialer.dial(adress);
+            } else {//Verify chcek fail
+                System.out.println("Invalid adres input.");
+                System.err.println("[Main]: Resseting program...");
+                main(null);//Restart the program
+            }
+        }
+
     }//End of main
-    public void writeMessage(String message){
+
+    public void writeMessage(String message) {
         System.out.println(message);
     }
 }//End of class
