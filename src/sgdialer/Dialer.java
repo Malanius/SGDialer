9556ba853a4e590ea022a7f304e294f4caad33c7
Malanius <malanius@seznam.cz>
2014-08-12 12:25:53 +0200

    Symbol pos location and dial methods

--- src/sgdialer/Dialer.java
+++ src/sgdialer/Dialer.java
@@ -6,6 +6,7 @@ package sgdialer;
  * @version 0.0
  */
 public class Dialer {
+    private int actualPos = 0;
     private char symbols[] = new char[38];//Symbols array
     //Consrtructor
     public Dialer(char[] symbols){
@@ -19,6 +20,27 @@ public class Dialer {
         System.err.println(strSymbols);
     }//End of Constructor
     public void dial(String adress){
+        char[] adressArray = adress.toCharArray();
+        for (int i = 0; i < adress.length(); i++){
+            System.err.printf("[Dialer]: Encoding symbol [%s]", adressArray[i]);
+            ringMove(calculateRoute(getSymbolPos(adressArray[i])));
+        }
+    }
+    
+    private int getSymbolPos(char symb){
+        int pos = -1;
+        do{
+            pos++;
+        }while (symbols[pos] == symb);
+        System.err.printf("[Dialer]: Found symbol %s at %s position.\n", symb, pos);
+        return pos;
+    }
+    private int calculateRoute(int pos){
+        int route;
+                
+        return route;
+    }
+    public void ringMove(int route){
         
     }
 }//End of class
